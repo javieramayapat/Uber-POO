95fbd2456e42a454c577f8c88c5a14fccee5ed27
Javier Amaya Patricio <javieramayapat@gmail.com>
2021-04-05 16:34:25 -0500

    update of JavaScript classes to ES6 standard

--- JS/Car.js
+++ JS/Car.js
@@ -1,12 +1,14 @@
-function Car(license, driver) {
-    this.id;
-    this.license = license;
-    this.driver = driver;
-    this.passenger;
+class Car {
+    constructor(license, driver) {
+        this.id;
+        this.license = license;
+        this.driver = driver;
+        this.passenger;
+    }
+    printDataCar = () => {
+        console.log("Licencia: " + this.license + "\n" +
+            "Nombre Conductor: " + this.driver.name + "\n" +
+            "Nombre documento: " + this.driver.document);
+    }
 }
 
-Car.prototype.printDataCar = function () {
-    console.log("Licencia: " + this.license + "\n" +
-        "Nombre Conductor: " + this.driver.name + "\n" +
-        "Nombre documento: " + this.driver.document);
-}
--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,9 @@
-var car =  new Car("ART9685", new Account("Mayra","ASERTTM147"));
+var car =  new Car("ART9685", new Account("Elvira","ASERTTM147"));
 car.passenger = 4;
 car.printDataCar();
 
 console.log(car);
 
-var car2 =  new Car("RTFG34", new Account("Jasmin","JAST587"));
+var car2 =  new Car("RTFG34", new Account("Rosario","JAST587"));
 car2.passenger = 5;
 car2.printDataCar();
