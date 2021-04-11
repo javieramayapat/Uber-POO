c409c813b3236d96f09630af822ac69ffc63b1ce
Javier Amaya Patricio <javieramayapat@gmail.com>
2021-04-11 01:02:50 -0500

    Apply hineritance in Python

--- Python/main.py
+++ Python/main.py
@@ -2,7 +2,13 @@ from car import Car
 from account import Account
 
 if __name__ == "__main__":
+    
     car = Car("ADR487", Account("Javier","Amaya"))
     car.passenger = 4;
     print(vars(car))
     print(vars(car.driver))
+    
+    car2 = Car("AMS234", Account("Andres Herrera", "ANDA876"))
+    car2.passenger = 5
+    print(vars(car2))
+    print(vars(car2.driver))
