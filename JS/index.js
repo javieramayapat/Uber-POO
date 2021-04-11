087962af518783fded3d85e9043fd31611c9ba61
Javier Amaya Patricio <javieramayapat@gmail.com>
2021-04-11 01:23:33 -0500

    Inheritance in JavaScript

--- JS/index.js
+++ JS/index.js
@@ -7,3 +7,7 @@ console.log(car);
 var car2 =  new Car("RTFG34", new Account("Rosario","JAST587"));
 car2.passenger = 5;
 car2.printDataCar();
+
+var uberx =  new UberX("FRTGV45", new Account("Josue","JUS543"),"X",2021);
+uberx.passenger = 6;
+uberx.printDataCar();
